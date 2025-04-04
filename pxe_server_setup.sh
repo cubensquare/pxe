91966e54253eea4cd7a003dd6b87eb761c13598e
cubensquare <66881155+cubensquarets@users.noreply.github.com>
2025-04-04 11:05:24 +0530

    Update pxe_server_setup.sh

--- pxe_server_setup.sh
+++ pxe_server_setup.sh
@@ -138,7 +138,7 @@ systemctl restart tftpd-hpa
 echo "[+] Downloading Debian Live ISO (XFCE 12.5.0)"
 mkdir -p /mnt/iso /var/www/html/debian/live /srv/tftp/debian
 if [ ! -f "~/debian.iso" ]; then
-  wget -O ~/debian.iso https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/debian-live-12.10.0-amd64-standard.iso
+  wget -O ~/debian.iso https://cdimage.debian.org/cdimage/archive/12.5.0-live/amd64/iso-hybrid/debian-live-12.5.0-amd64-xfce.iso
 else
   echo " Debian ISO already downloaded. Skipping."
 fi
